a212ef04e508bddff8d77b5a21bab6181e527103
nikita2060 <nikkyapandey@gmail.com>
2023-09-26 15:39:06 +0530

    �:lock:Adding environment variables for API username and key

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import requests
 from datetime import datetime
+import os
 
 today = datetime.now()
 date = today.strftime("%Y%m%d")
 
-USERNAME = "nikky"
-TOKEN = "qwertyuiop"
+USERNAME = os.environ.get("PIXELA_USER")  # using environment variables is appreciated for security reasons
+TOKEN = os.environ.get("PIXELA_TOKEN")
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
 
 user_params = {
-    "token": "qwertyuiop",
+    "token": TOKEN,
     "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
@@ -16,8 +20,8 @@ user_params = {
 create_user_endpoint = "https://pixe.la/v1/users"
 
 
-# response = requests.post(url=create_user_endpoint, json=user_params)
-# print(response.json())
+response = requests.post(url=create_user_endpoint, json=user_params, headers=headers)
+print(response.json())
 
 graph_params = {
     "id": "graph1",
@@ -28,15 +32,22 @@ graph_params = {
 }
 create_graph_endpoint = f"{create_user_endpoint}/{USERNAME}/graphs"
 
-headers = {
-    "X-USER-TOKEN": TOKEN
-}
+
 # response = requests.post(url=create_graph_endpoint, json=graph_params, headers=headers)
 
-create_pixel_endpoint = f"{create_user_endpoint}/{USERNAME}/graphs/graph1"
+create_pixel_endpoint = f"{create_graph_endpoint}/graph1"
+# print(create_pixel_endpoint)
 pixel_params = {
     "date": date,
     "quantity": "1"
 }
-response = requests.post(url=create_pixel_endpoint, json=pixel_params, headers=headers)
-print(response.text)
+# response = requests.post(url=create_pixel_endpoint, json=pixel_params, headers=headers)
+# print(response.text)
+
+update_pixel_endpoint = f"{create_pixel_endpoint}/{date}"
+pixel_data = {
+    "quantity": "2"
+}
+# response = requests.put(url=update_pixel_endpoint, json=pixel_data, headers=headers)
+# print(update_pixel_endpoint)
+# print(response.text)
