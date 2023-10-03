7ceb287482c3a92d88470e0aa28652ed8a85b997
nikita2060 <nikkyapandey@gmail.com>
2023-10-03 22:31:04 +0530

    Applying modular approach to optimize program

--- main.py
+++ main.py
@@ -2,52 +2,64 @@ import requests
 from datetime import datetime
 import os
 
-today = datetime.now()
-date = today.strftime("%Y%m%d")
 
-USERNAME = os.environ.get("PIXELA_USER")  # using environment variables is appreciated for security reasons
-TOKEN = os.environ.get("PIXELA_TOKEN")
-headers = {
-    "X-USER-TOKEN": TOKEN
-}
-
-user_params = {
-    "token": TOKEN,
-    "username": USERNAME,
-    "agreeTermsOfService": "yes",
-    "notMinor": "yes"
-}
-create_user_endpoint = "https://pixe.la/v1/users"
+# program written in modular form is more readable and optimum
+def create_user(username, token):
+    headers = {"X-USER-TOKEN": TOKEN}
+    user_params = {
+        "token": TOKEN,
+        "username": USERNAME,
+        "agreeTermsOfService": "yes",
+        "notMinor": "yes"
+    }
+    create_user_endpoint = "https://pixe.la/v1/users"
+    response = requests.post(url=create_user_endpoint, json=user_params, headers=headers)
+    print(response.json())
 
 
-response = requests.post(url=create_user_endpoint, json=user_params, headers=headers)
-print(response.json())
+def create_graph(username, token):
+    headers = {"X-USER-TOKEN": TOKEN}
+    graph_params = {
+        "id": "graph1",
+        "name": "Coding graph ",
+        "unit": "Program",
+        "type": "int",
+        "color": "ajisai"
+    }
+    create_graph_endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs"
+    response = requests.post(url=create_graph_endpoint, json=graph_params, headers=headers)
+    print(response.json())
 
-graph_params = {
-    "id": "graph1",
-    "name": "Coding graph ",
-    "unit": "Program",
-    "type": "int",
-    "color": "ajisai"
-}
-create_graph_endpoint = f"{create_user_endpoint}/{USERNAME}/graphs"
+
+def create_pixel(username, token, today_date, input_quantity):
+    headers = {"X-USER-TOKEN": TOKEN}
+    create_pixel_endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs/graph1"
+    pixel_params = {
+        "date": today_date,
+        "quantity": input_quantity
+    }
+    response = requests.post(url=create_pixel_endpoint, json=pixel_params, headers=headers)
+    print(response.text)
 
 
-# response = requests.post(url=create_graph_endpoint, json=graph_params, headers=headers)
+def update_pixel(username, token, today_date, input_quantity):
+    headers = {"X-USER-TOKEN": TOKEN}
+    update_pixel_endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs/graph1/{date}"
+    pixel_data = {
+        "quantity": input_quantity
+    }
+    response = requests.put(url=update_pixel_endpoint, json=pixel_data, headers=headers)
+    print(response.text)
 
-create_pixel_endpoint = f"{create_graph_endpoint}/graph1"
-# print(create_pixel_endpoint)
-pixel_params = {
-    "date": date,
-    "quantity": "1"
-}
-# response = requests.post(url=create_pixel_endpoint, json=pixel_params, headers=headers)
-# print(response.text)
 
-update_pixel_endpoint = f"{create_pixel_endpoint}/{date}"
-pixel_data = {
-    "quantity": "2"
-}
-# response = requests.put(url=update_pixel_endpoint, json=pixel_data, headers=headers)
-# print(update_pixel_endpoint)
-# print(response.text)
+if __name__ == "__main__":
+    today = datetime.now()
+    date = today.strftime("%Y%m%d")
+    USERNAME = os.environ.get("PIXELA_USER")  # using environment variables is appreciated for security reasons
+    TOKEN = os.environ.get("PIXELA_TOKEN")
+    quantity = input("How many programs did you do today?")   # asking user input for customization
+
+    print(create_user(USERNAME, TOKEN))
+    print(create_graph(USERNAME, TOKEN))
+    print(create_pixel(USERNAME, TOKEN, date, quantity))
+    print(update_pixel(USERNAME, TOKEN, date, quantity))
